da5ff6870cbf37fb62e9c1ffae7c7372191a45b2
jueonkang <167186841+jueonkang@users.noreply.github.com>
2024-05-07 13:09:30 +0900

    [Bronze III] Title: 윷놀이, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,12 @@
+for i in range(3):
+  a = sum(map(int, input().split()))
+  if a == 4:
+      print("E")
+  elif a == 3:
+      print("A")
+  elif a == 2:
+      print("B")
+  elif a == 1:
+      print("C")
+  else:
+      print("D")
