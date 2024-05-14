9e2c36c971189e550b2a1ba7e706a4ada7f81209
jueonkang <167186841+jueonkang@users.noreply.github.com>
2024-05-14 17:18:16 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 256 ms, Memory: 110916 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -1,13 +1,15 @@
-
-def isPalindrome(s, l, r,cnt):
-  if l >= r: return 1 , cnt
-  elif s[l] != s[r]: return 0, cnt
-  else: return isPalindrome(s, l+1, r-1,cnt+1)
+def isPalindrome(s, l, r):
+  global cnt
+  cnt += 1 
+  if l >= r: return 1 
+  elif s[l] != s[r]: return 0
+  else: return isPalindrome(s, l+1, r-1)
   
 
 a = int(input())
+
 for i in range(a):
   b = input()
-  c = list(isPalindrome(b,0,len(b)-1,1))
-  print(c[0], c[1])
-
+  cnt = 0
+  c = isPalindrome(b,0,len(b)-1)
+  print(c, cnt)
