c2a481df7a9564864c30618e1bc63a0a912af559
jueonkang <167186841+jueonkang@users.noreply.github.com>
2024-05-16 16:34:03 +0900

    [Bronze III] Title: 부호, Time: 368 ms, Memory: 115116 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,9 @@
+for i in range(3):
+    N = int(input())
+    li = [int(input()) for i in range(N)]
+    if sum(li) == 0:
+        print("0")
+    elif sum(li) > 0:
+        print("+")
+    else:
+        print("-")
