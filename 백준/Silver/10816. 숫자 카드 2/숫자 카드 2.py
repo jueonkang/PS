2542f561182285f2779800a88290c00d14372c87
jueonkang <167186841+jueonkang@users.noreply.github.com>
2024-06-06 18:03:04 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 652 ms, Memory: 243636 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,20 @@
+n = int(input())
+card = list(map(int,input().split()))
+m =int(input())
+count = map(int,input().split())
+
+card.sort()
+a = {}
+
+for x in card: 
+  if x in a:
+    a[x] += 1
+  else:
+    a[x] = 1
+
+for x in count:
+  if x in a:
+    print(a[x], end= " ")
+  else:
+    print("0", end = " ")
+
